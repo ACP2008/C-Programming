37ec611519198ba6e06e693079ff0277565d9c63
ACP2008 <ayushprajapti1610@gmail.com>
2026-07-19 11:31:20 +0530

    Add Fibonacci series loop implementation

---
+++ 03-Loops/fibonacci_loop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int number, first = 0, second = 1, next, i;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &number);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= number; i++)
+    {
+        printf("%d ", first);
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    printf("\n");
+
+    return 0;
+}
