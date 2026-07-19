cbb500d2e2285db837d70ffdba4b79361fba9c6f
ACP2008 <ayushprajapti1610@gmail.com>
2026-07-19 11:36:14 +0530

    Update array_sum.c

---
+++ 04-Arrays/array_sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+    int i, sum = 0;
+
+    printf("Enter 5 elements:\n");
+
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        sum = sum + arr[i];
+    }
+
+    printf("Sum of Array Elements = %d\n", sum);
+
+    return 0;
+}
